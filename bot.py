bf1420afc5cdf97d25c712ec199e70b14ea0e973
nnirann <45460439+nnirann@users.noreply.github.com>
2021-12-15 13:01:25 +0530

    circle nick

--- bot.py
+++ bot.py
@@ -326,14 +326,11 @@ async def nick(ctx):
 
 
 bot.nick_count = 0
-nick_list = ["◜",
-			"◠",
-			"◝",
-			"◞",
-			"◡",
-			"◟"]
+nick_list = ["◡",
+			"⊙",
+			"◠"]
 
-@tasks.loop(seconds=0.5)
+@tasks.loop(seconds=0.2)
 async def nick_loop():
     server = bot.get_guild(772345603400531988)
     member = await server.fetch_member(524200058686799903)
